e1ea5e4cbba293e1155dc95af1d98e469ff856db
IbrarHussain123 <117770517+IbrarHussain123@users.noreply.github.com>
2022-11-08 15:44:59 -0500

    even.java

---
+++ even.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class evenodd{
+  public static void main(String[] args) {
+  	 Scanner sc=new Scanner(System.in);
+      	
+      System.out.println("Enter Any Number:");
+     int n=sc.nextInt();   
+    if(n%2==0){
+      System.out.println(n+":its even Number:");
+    }
+    else{
+      System.out.println(n+":its odd Number:");
+    }
+
+    
+  }
+  
+
+
+
+
+
+}

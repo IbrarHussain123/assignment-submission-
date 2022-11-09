145f9961a7ddb532f111b7566957e3321db75c7a
IbrarHussain123 <117770517+IbrarHussain123@users.noreply.github.com>
2022-11-09 09:14:26 -0500

    Add files via upload

---
+++ single.java
@@ -0,0 +1,17 @@
+ class test{
+
+	public static void main(String args[]){
+
+    String name_1="ibrar";
+    String name_2="hussain";
+
+
+    System.out.println("printing a single variable");
+    System.out.printf("%s",name_1);
+    System.out.println("\nprinting multiple variable");
+	System.out.printf("First name:%s\nLast Name:%s",name_1,name_2);
+	}
+
+
+
+}
